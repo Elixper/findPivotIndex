a8e93757fbe7743fb6587669e20db32d402c3cb2
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2023-01-16 17:57:56 +0100

    second attempt

--- findPivotIndex.js
+++ findPivotIndex.js
@@ -2,15 +2,31 @@
  * @param {number[]} nums
  * @return {number}
  */
- var pivotIndex = function(nums) {
-    let i = Math.floor((nums.length)/2)
-    console.log(i)
-    let leftArr = nums.splice(0,i)
-    let rightArr = nums.splice(i-nums.length+1,nums.length)
-    console.log(leftArr)
-    console.log(rightArr)
+var pivotIndex = function (nums) {
+  let i = Math.floor(nums.length / 2);
+  let newNums = [...nums];
+  let newNums2 = [...nums];
+  console.log(i);
+  let leftArr = newNums.splice(0, i);
+  let rightArr = newNums2.splice(i + 1, nums.length);
+  let accum = (arr) => arr.reduce((acc, curr) => acc + curr);
+  if (accum(leftArr) === accum(rightArr)) {
+    return i;
+  } else if (accum(leftArr) > accum(rightArr)) {
+     i = i-1;
+     console.log(i)
+     console.log(accum(leftArr))
+  } else {
+     i = i+1;
+     console.log(i)
+     console.log(accum(leftArr))
+  }
+  console.log(accum(leftArr));
+  console.log(accum(rightArr));
+  console.log(leftArr);
+  console.log(rightArr);
 };
 
-console.log(pivotIndex([1,7,3,6,5,6]))
-console.log(pivotIndex([1,2,3]))
-console.log(pivotIndex([2,1,-1]))
+console.log(pivotIndex([1, 7, 3, 6, 5, 6]));
+console.log(pivotIndex([1, 2, 3]));
+console.log(pivotIndex([2, 1, -1]));
